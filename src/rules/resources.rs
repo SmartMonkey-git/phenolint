2eab26d712041fdc0e046ec714cda44fc69bd7f9
Daniel Danis <daniel.gordon.danis@protonmail.com>
2025-11-27 12:12:34 +0100

    Test the rule.

--- src/rules/resources.rs
+++ src/rules/resources.rs
@@ -1,9 +1,9 @@
-use crate::LinterContext;
 use crate::diagnostics::LintViolation;
 use crate::error::FromContextError;
 use crate::rules::rule_registration::RuleRegistration;
 use crate::rules::traits::{LintRule, RuleCheck, RuleFromContext, RuleMetaData};
 use crate::tree::node_repository::List;
+use crate::LinterContext;
 use phenolint_macros::register_rule;
 use phenopackets::schema::v2::core::{OntologyClass, Resource};
 use std::collections::HashSet;
@@ -50,6 +50,40 @@ impl RuleCheck for CuriesHaveResourcesRule {
             }
         }
         violations
+    }
+}
+
+#[cfg(test)]
+mod test_curies_have_resources {
+    use crate::rules::resources::CuriesHaveResourcesRule;
+    use crate::rules::traits::{RuleCheck, RuleMetaData};
+    use crate::tree::node::MaterializedNode;
+    use crate::tree::node_repository::List;
+    use crate::tree::pointer::Pointer;
+    use phenopackets::schema::v2::core::OntologyClass;
+
+    #[test]
+    fn check_that_a_term_needs_a_resource() {
+        let rule = CuriesHaveResourcesRule;
+
+        let ocs = [MaterializedNode {
+            materialized_node: OntologyClass {
+                id: "HP:0001250".into(),
+                label: "Seizure".into(),
+            },
+            spans: Default::default(),
+            pointer: Pointer::new("/phenotypicFeatures/0/type"),
+        }];
+        let resources = [];
+        let data = (List(&ocs), List(&resources));
+
+        let violations = rule.check(data);
+
+        assert_eq!(violations.len(), 1);
+        let violation = violations.first().unwrap();
+
+        assert_eq!(violation.rule_id(), rule.rule_id());
+        assert_eq!(violation.at().first().unwrap().position(), "/phenotypicFeatures/0/type");
     }
 }
 
