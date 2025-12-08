1a52d95013f75d2b8ae83880f22e11fbd65d3f8f
SmartMonkey <rouven.reuter91@gmail.com>
2025-12-08 12:48:16 +0100

    Better message

--- src/tree/error.rs
+++ src/tree/error.rs
@@ -2,6 +2,6 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum NodeRepositoryError {
-    #[error("Cant Reinstantiate Node at '{0}' and type '{1}'.")]
+    #[error("Cant Reinstantiate Node at '{0}' with type '{1}'.")]
     CantReinstantiateNode(String, String),
 }
